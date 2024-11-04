5e01ad39eb30bd09fdfaf7fbec4eb95c9ddc61ed
ParkYongSoo <kmanpysev20@gmail.com>
2024-11-04 11:17:46 +0900

    v0.07

--- src/js/main.js
+++ src/js/main.js
@@ -5,6 +5,9 @@ $(document).ready(function () {
 function init() {
   mainSwiper1();
   toggleBackgroundColor();
+  leftClick();
+  rightClick();
+  cancelClick();
 }
 
 function mainSwiper1() {
@@ -59,3 +62,52 @@ function toggleBackgroundColor() {
   }
   // 클로저 사용
   setInterval(toggleBackgroundColor(), 3000);
+
+  function fullPagesLeft() {
+    $('.left-section #fullpage').fullpage({
+      scrollingSpeed: 1000,
+      menu: '.menu-left', // 앵커 링크를 위한 메뉴 ID
+      anchors: ['firstPage', 'secondPage', '3rdPage'], // 각 섹션의 앵커 이름
+    })
+  }
+
+  function leftClick() {
+    $('.left-circle').click(function(){
+      $('.left-section').addClass('active').children().css('opacity', '1');
+      $('body').addClass('scroll-none');
+      setTimeout(function() {
+        $('.cancel-btn').addClass('active');
+      }, 1000);
+      fullPagesLeft();
+    })
+  }
+
+  function fullPagesRight() {
+    $('.right-section #fullpage').fullpage({
+      scrollingSpeed: 1000,
+      menu: '.menu-right', // 앵커 링크를 위한 메뉴 ID
+      anchors: ['firstPages', 'secondPages', '3rdPages'], // 각 섹션의 앵커 이름
+    })
+  }
+  function rightClick() {
+    $('.right-circle').click(function(){
+      $('.right-section').addClass('active').children().css('opacity', '1');
+      $('body').addClass('scroll-none');
+      setTimeout(function() {
+        $('.cancel-btn').addClass('active');
+      }, 1000);
+      fullPagesRight();
+    })
+  }
+
+  function cancelClick() {
+    $('.cancel-btn').click(function(){
+      $('.left-section,.right-section').removeClass('active').children().css('opacity', '0');
+      $('.cancel-btn').removeClass('active');
+      $('body').removeClass('scroll-none');
+      // fullpage에서 추가된 클래스와 스타일 제거
+      $('html, body').removeClass('fp-enabled fp-viewing fp-responsive fp-viewing-0').removeAttr('style');
+      $.fn.fullpage.destroy('all');
+    });
+  }
+  
